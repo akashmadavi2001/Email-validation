e3112e4af1c3387d7ab876ffdb925576d3a289af
Akash Madavi <akashmadavi2001@gmail.com>
2025-03-05 09:03:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let input = document.querySelector('.container input');
+let img = document.querySelector('.container img');
+
+input.addEventListener('keyup', () => {
+    
+    let pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,3}))$/;
+
+    if (input.value === '') {
+        img.src = "email.png";
+        img.classList.remove("active");
+    }
+    else if (input.value.match(pattern)) {
+        img.src = "checkmark.png";
+    }
+    else {
+        img.src = "message.png";
+        img.classList.add("active");
+    }
+})
